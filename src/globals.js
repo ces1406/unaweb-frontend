a7967bb3386fad8242753f3e7c79f5a8392b085a
cesar-win10-pc <ces1406@gmail.com>
2022-02-15 19:21:59 -0300

    fixing server's url

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,5 @@
 export const ITEMS_POR_PAG = 12;
-export const SERVER_URL = 'http://localhost:5000';//https://unaweb.herokuapp.com';
+export const SERVER_URL = 'https://unaweb.herokuapp.com';
 export const LINK_YOUTUBE = 'https://www.youtube.com/channel/UClfv3GTM04aMzPgnabbhG0w?view_as=subscriber';
 export const LINK_TWITTER = 'https://twitter.com/UNA_visuales';
 export const LINK_FACEBOOK = 'https://www.facebook.com/groups/iuna.artesvisuales/';
