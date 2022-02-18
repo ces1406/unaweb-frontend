5da8b6b5ce56714cb0d2baaedd8d1eb6646e2068
cesar-win10-pc <ces1406@gmail.com>
2022-02-17 22:06:16 -0300

    changing globals

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,6 @@
 export const ITEMS_POR_PAG = 12;
-export const SCREEN_SMALL = 820;
+export const SCREEN_SM = 820;
+export const SCREEN_MD = 1024;
 export const SERVER_URL = 'https://unaweb.herokuapp.com';
 export const LINK_YOUTUBE = 'https://www.youtube.com/channel/UClfv3GTM04aMzPgnabbhG0w?view_as=subscriber';
 export const LINK_TWITTER = 'https://twitter.com/UNA_visuales';
