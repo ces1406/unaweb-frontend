b9d3ddef523c08c9ac9e04eb9167180396adaa0f
cesar-win10-pc <ces1406@gmail.com>
2023-08-23 15:41:05 -0300

    add ico image

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports={
                 use:['style-loader','css-loader']
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif|webp)$/,
+                test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/,
                 use: ['file-loader']
               }
         ]
